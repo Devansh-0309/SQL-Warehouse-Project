1484a24e0afa539d967b4852b30bc7b6f5710847
Devansh Maheshwari <154952403+Devansh-0309@users.noreply.github.com>
2025-04-17 15:38:30 +0530

    Create CleanErpCustAz12.sql

---
+++ Scripts/Silver/CleanErpCustAz12.sql
@@ -0,0 +1,39 @@
+/*
+==============================================================
+Cleaning the data of Bronze.erp_cust_az12
+==============================================================
+
+Purpose of the Script
+	This Piece of code cleans the table by following:
+		1. Removes unwanted characters 
+		2. Removes empty spaces from the string type columns
+		3. Stardardization of columns into something meaningful
+		4. Cleans the invalid inputs
+
+	Then this script inserts the cleaned data to the table in the silver layer
+*/
+
+-- Inserting Data into the silver layer table
+INSERT INTO Silver.erp_cust_az12(cid, bdate, gen)
+
+SELECT
+-- Removing the unwanted characters
+CASE 
+	WHEN cid LIKE 'NAS%' THEN SUBSTRING(cid, 4, LEN(cid))
+	ELSE cid
+END AS cid,
+
+-- Removing invalid dates
+CASE 
+	WHEN bdate > GETDATE() THEN NULL
+	ELSE bdate
+END AS bdate,
+
+-- Handling and converting abbrevations to full forms
+CASE 
+	WHEN UPPER(TRIM(gen)) IN ('F', 'FEMALE') THEN 'Female'
+	WHEN UPPER(TRIM(gen)) IN ('M', 'MALE') THEN 'Male'
+	ELSE 'n/a'
+END AS gen
+FROM Bronze.erp_cust_az12
+
